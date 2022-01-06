81ff4da9b461edce31fdd8482e68a6a09d47f3d1
Jonatan Murcia <jonatanjosmurcia@gmail.com>
2022-01-05 23:42:14 -0500

    Main README

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const config = {
     port: process.env.PORT || 3000,
     dbName : process.env.DB_NAME,
-    dbUser : process.env.DB_USER,
     dbPassword : process.env.DB_PASSWORD,
-    collection : process.env.COLLECTION_NAME,
     mongoConnection: process.env.MONGO_CONNECTION,
     secret: process.env.JWTSECRET
 }; 
